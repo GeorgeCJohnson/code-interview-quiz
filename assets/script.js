4a5a6e6bb250a480006dc2946ececea28156dfdb
George J <Gjohnson.pioneer@gmail.com>
2023-07-20 12:48:26 -0400

    Add for loop for answer selection.

--- assets/script.js
+++ assets/script.js
@@ -12,25 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
             question: 'What is the correct syntax for referring to an external script called "script.js"?',
             answers: ['<script src="script.js">', '<script href="script.js">', '<script ref="script.js">', '<script name="script.js">'],
             correctAnswer: 0,
-        }
+        },
 
         {
             question: 'What is the NaN property of the global object?',
             answers: ['Not a Number', 'Not a Null', 'Not a Negative', 'Not a Negative'],
             correctAnswer: 0,
-        }
+        },
 
         {   question: 'What are callbacks?',
             answers: ['A function that is passed into another function as an argument', 'A function that is called after another function has finished executing', 'A function that is called before another function has finished executing', 'A function that is called before another function has started executing'],
             correctAnswer: 0,
-        }
+        },
 
         {   question: 'What is the difference between call() and apply()?',
             answers: ['The call() method takes arguments separately whereas the apply() method takes arguments as an array', 'The call() method takes arguments as an array whereas the apply() method takes arguments separately', 'The call() method takes arguments as an array whereas the apply() method takes arguments as an array', 'The call() method takes arguments separately whereas the apply() method takes arguments separately'],
             correctAnswer: 0,
         },
-    ];
-    
+        ];
+        
     var questionIndex = 0;
     var time = questions.length * 15;
     var timeInterval;
@@ -45,4 +45,11 @@ document.addEventListener('DOMContentLoaded', function() {
     //Questions retrieved from DOM
     var questionEl = document.getElementById('question');
 
-    
+    //Answers selection by User
+    var answerButtonsEl = document.getElementById('answerButtons');
+    var resultEl = document.getElementById('result');
+
+    for (var i = 0; i < answerButtons.length; i++) {
+        answerButtonsEl[1].setAttribute('id', 'answer-buttons' + i);
+        answerButtons[i].addEventListener('click', checkAnswers);
+    }
