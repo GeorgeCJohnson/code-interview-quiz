8de9b5c6dcef1a18709c3b014bd43dadea74356d
George J <Gjohnson.pioneer@gmail.com>
2023-08-02 13:02:52 -0400

    Final commit

--- assets/script.js
+++ assets/script.js
@@ -1,170 +1,193 @@
 //Quiz opens on page load
-document.addEventListener ('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var questions = [
+    {
+      question: "What mode should you run JavaScript in to avoid hoisting?",
+      answers: ["Strict", "Sloppy", "Strict and Sloppy", "None of the above"],
+      correctAnswer: 0,
+    },
 
-    var questions = [
+    {
+      question:
+        'What is the correct syntax for referring to an external script called "script.js"?',
+      answers: [
+        '<script src="script.js">',
+        '<script href="script.js">',
+        '<script ref="script.js">',
+        '<script name="script.js">',
+      ],
+      correctAnswer: 0,
+    },
 
-        {
-            question: 'What mode should you run JavaScript in to avoid hoisting?',
-            answers: ['Strict', 'Sloppy', 'Strict and Sloppy', 'None of the above'],
-            correctAnswer: 0,
-        },
+    {
+      question: "What is the NaN property of the global object?",
+      answers: ["Not a Number", "Not a Null", "Not a Negative", "Not a Ninja"],
+      correctAnswer: 0,
+    },
 
-        {
-            question: 'What is the correct syntax for referring to an external script called "script.js"?',
-            answers: ['<script src="script.js">', '<script href="script.js">', '<script ref="script.js">', '<script name="script.js">'],
-            correctAnswer: 0,
-        },
+    {
+      question: "What are callbacks?",
+      answers: [
+        "A function that is passed into another function as an argument",
+        "A function that is called after another function has finished executing",
+        "A function that is called before another function has finished executing",
+        "A function that is called before another function has started executing",
+      ],
+      correctAnswer: 0,
+    },
 
-        {
-            question: 'What is the NaN property of the global object?',
-            answers: ['Not a Number', 'Not a Null', 'Not a Negative', 'Not a Ninja'],
-            correctAnswer: 0,
-        },
+    {
+      question: "What is the difference between call() and apply()?",
+      answers: [
+        "The call() method takes arguments separately whereas the apply() method takes arguments as an array",
+        "The call() method takes arguments as an array whereas the apply() method takes arguments separately",
+        "The call() method takes arguments as an array whereas the apply() method takes arguments as an array",
+        "The call() method takes arguments separately whereas the apply() method takes arguments separately",
+      ],
+      correctAnswer: 0,
+    },
+  ];
 
-        {
-            question: 'What are callbacks?',
-            answers: ['A function that is passed into another function as an argument', 'A function that is called after another function has finished executing', 'A function that is called before another function has finished executing', 'A function that is called before another function has started executing'],
-            correctAnswer: 0,
-        },
+  var endScreenEl = document.getElementById("end-screen");
+  var questionIndex = 0;
+  var time = 60;
+  var timeInterval;
+  var startButton = document.getElementById("start-button");
+  var answerButtonsEl = document.getElementById("answerButtons");
+  var resultEl = document.getElementById("result");
+  var timerEl = document.getElementById("time");
+  var initialsInput = document.getElementById("initials");
+  var submitButton = document.getElementById("submit");
 
-        {
-            question: 'What is the difference between call() and apply()?',
-            answers: ['The call() method takes arguments separately whereas the apply() method takes arguments as an array', 'The call() method takes arguments as an array whereas the apply() method takes arguments separately', 'The call() method takes arguments as an array whereas the apply() method takes arguments as an array', 'The call() method takes arguments separately whereas the apply() method takes arguments separately'],
-            correctAnswer: 0,
-        },
-    ];
+  //Timer display on page
+  function clockTick() {
+    time--;
+    if (time <= 0) {
+      clearInterval(timeInterval);
+      endQuiz();
+      timerEl.textContent = time;
+    }
+    timerEl.textContent = time;
+  }
 
-    var questionIndex = 0;
-    var time = 60;
-    var timeInterval;
-    var startButton = document.getElementById("start-button");
-    var answerButtonsEl = document.getElementById('answerButtons');
-    var resultEl = document.getElementById('result');
-    var timerEl = document.getElementById('time');
-    var initialsInput = document.getElementById('initials');
-    var submitButton = document.getElementById('submit');
+  //starting the quiz
+  function startQuiz() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.style.display = "none";
 
-    //Timer display on page
-    function clockTick() {
-        time--;
-        if (time <= 0) {
-            clearInterval(timeInterval);
-            endQuiz();
-            timerEl.textContent = time;
-        }
-        timerEl.textContent = time;
+    var quizSection = document.querySelector(".quiz");
+    quizSection.style.display = "block";
+
+    var questionEl = document.getElementById("question");
+    questionEl.style.display = "block";
+    timeInterval = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    displayQuestion();
+  }
+
+  startButton.addEventListener("click", startQuiz);
+
+  function displayQuestion() {
+    var questionTextEl = document.getElementById("question-text");
+    var question = questions[questionIndex];
+
+    questionTextEl.textContent = question.question;
+    answerButtonsEl.innerHTML = "";
+
+    for (var i = 0; i < question.answers.length; i++) {
+      var answerButton = document.createElement("button");
+      answerButton.setAttribute("class", "data-answer");
+      answerButton.setAttribute("data-answer", i);
+      answerButton.textContent = question.answers[i];
+      answerButtonsEl.appendChild(answerButton);
     }
 
-    //starting the quiz
-    function startQuiz() {
-        var startScreenEl = document.getElementById('start-screen');
-        startScreenEl.style.display = 'none';
-
-        var quizSection = document.querySelector('.quiz');
-        quizSection.style.display = 'block';
-
-        var questionEl = document.getElementById('question');
-        questionEl.style.display = 'block';
-        timeInterval = setInterval(clockTick, 1000);
-        timerEl.textContent = time;
-        displayQuestion();
-    }
-
-    startButton.addEventListener('click', startQuiz);
-
-    function displayQuestion() {
-        var questionTextEl = document.getElementById('question-text');
-        var question = questions[questionIndex];
-
-        questionTextEl.textContent = question.question;
-        answerButtonsEl.innerHTML = '';
-
-        for (var i = 0; i < question.answers.length; i++) {
-            var answerButton = document.createElement('button');
-            answerButton.setAttribute('class', 'data-answer');
-            answerButton.setAttribute('data-answer', i);
-            answerButton.textContent = question.answers[i];
-            answerButtonsEl.appendChild(answerButton);
-        }
-
-// Attach event listener to answer buttons
+    // Attach event listener to answer buttons
     for (var i = 0; i < answerButtonsEl.children.length; i++) {
-    answerButtonsEl.children[i].addEventListener('click', checkAnswers);
-        }
+      answerButtonsEl.children[i].addEventListener("click", checkAnswers);
     }
+  }
 
-//Tmeout for wrong answers
-    function checkAnswers(event) {
-        var answer = event.target.getAttribute('data-answer');
-        var result = document.getElementById('result');
+  //Tmeout for wrong answers
+  function checkAnswers(event) {
+    var answer = event.target.getAttribute("data-answer");
+    var result = document.getElementById("result");
 
-        if (parseInt(answer, 10) === questions[questionIndex].correctAnswer) {
-            result.textContent = 'Correct!';
-        } else {
-            result.textContent = 'Take time to study this content!';
-            time -= 10;
-        }
-        questionIndex++;
-        if (questionIndex === questions.length) {
-            endQuiz();
-        } else {
-            displayQuestion();
-        }
+    if (parseInt(answer, 10) === questions[questionIndex].correctAnswer) {
+      result.textContent = "Correct!";
+    } else {
+      result.textContent = "Take time to study this content!";
+      time -= 10;
     }
+    questionIndex++;
+    if (questionIndex === questions.length) {
+      endQuiz();
+    } else {
+      displayQuestion();
+    }
+  }
 
-// Function to show high score results
-    function showHighscores() {
-    var highScores = JSON.parse(localStorage.getItem('highscores')) || [];
-    var highscoreListEl = document.getElementById('highscorelist');
-    highscoreListEl.innerHTML = '';
-  
+  // Function to show high score results
+  function showHighscores() {
+    endScreenEl.classList.add("hide");
+    var highScoreContainer = document.getElementById("highscore");
+    var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+    var highscoreListEl = document.getElementById("highscorelist");
+    highscoreListEl.innerHTML = "";
+    highScoreContainer.classList.remove("hide");
+
     highScores.sort(function (a, b) {
       return b.score - a.score;
     });
-  
+
     for (var i = 0; i < highScores.length; i++) {
       var highScoreEl = document.createElement("tr");
-      highScoreEl.innerHTML = "<td>" + highScores[i].score + "</td><td>" + highScores[i].initials + "</td>";
+      highScoreEl.innerHTML =
+        "<td>" +
+        highScores[i].score +
+        "</td><td>" +
+        highScores[i].initials +
+        "</td>";
       highscoreListEl.appendChild(highScoreEl);
     }
-    } 
+  }
 
-// Saving the score
-var submitButton = document.getElementById('submit');
-function saveScore() {
+  // Saving the score
+  var submitButton = document.getElementById("submit");
+  function saveScore() {
     var initials = initialsInput.value.trim();
-        if (initials !== '') {
-            var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
-            var newScore = {
-                score: time,
-                initials: initials,
-            };
-    highscores.push(newScore);
-        localStorage.setItem('highscores', JSON.stringify(highscores));
-        showHighscores();
-        }
+    if (initials !== "") {
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+      var newScore = {
+        score: time,
+        initials: initials,
+      };
+      highscores.push(newScore);
+      localStorage.setItem("highscores", JSON.stringify(highscores));
+      showHighscores();
     }
+  }
 
-submitButton.addEventListener('click', saveScore);
+  submitButton.addEventListener("click", saveScore);
 
-//Ending the quiz
-function endQuiz() {
+  //Ending the quiz
+  function endQuiz() {
     clearInterval(timeInterval);
-    var endScreenEl = document.getElementById('end-screen');
-    endScreenEl.style.display = 'block';
-
-    var finalScoreEl = document.getElementById('final-score');
+   
+    endScreenEl.classList.remove ("hide");
+    
+    var finalScoreEl = document.getElementById("final-score");
     finalScoreEl.textContent = time;
 
-    var questionEl = document.getElementById('question');
-    questionEl.style.display = 'none';
-    }
+    var questionEl = document.getElementById("question");
+    questionEl.style.display = "none";
+  }
 
-var buttonEl = document.querySelector("#clear-highscore")
-    buttonEl.addEventListener("click", function () {
-    localStorage.removeItem("Highscore")
-    var highScorelist = document.querySelector("#highscorelist")
+  var buttonEl = document.querySelector("#clear-highscore");
+  buttonEl.addEventListener("click", function () {
+    localStorage.removeItem("Highscore");
+    var highScorelist = document.querySelector("#highscorelist");
     highScorelist.innerHTML = "";
-    })
-
+  });
 });
